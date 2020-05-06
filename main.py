1388e502547f75f03d1e8d882e3ff3109b4d3111
Andreas Gropp <starmaerker@googlemail.com>
2020-05-06 21:53:27 +0200

    summarize the dataset

--- main.py
+++ main.py
@@ -14,3 +14,17 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+
+# Load dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# shape
+print(dataset.shape)
+# head
+print(dataset.head(20))
+# descriptions
+print(dataset.describe())
+# class distribution
+print(dataset.groupby('class').size())
